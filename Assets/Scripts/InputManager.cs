7de1a1992a9ac36b84ad769265bfda2a8de08e8a
prussia1891 <prussia1891@gmail.com>
2025-02-27 18:24:57 -0800

    player movement and jump

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent OnSpacePressed = new UnityEvent();
+    public UnityEvent OnResetPressed = new UnityEvent();
+
+    private void Update()
+    {
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            input += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            input += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            input += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input += Vector2.right;
+        }
+        OnMove?.Invoke(input);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            OnResetPressed?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private float dashForce = 10f;
+    [SerializeField] private float dashCooldown = 1f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+    private bool canDoubleJump;
+    private bool canDash = true;
+
+    private void Start()
+    {
+        inputManager.OnMove.AddListener(MovePlayer);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpPlayer();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            DashPlayer();
+        }
+    }
+
+    private void MovePlayer(Vector2 direction)
+    {
+        Vector3 moveDirection = new Vector3(direction.x, 0f, direction.y);
+        rb.AddForce(speed * moveDirection);
+    }
+
+    private void JumpPlayer()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+            canDoubleJump = true;
+        }
+        else if (canDoubleJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            canDoubleJump = false;
+        }
+    }
+
+    private void DashPlayer()
+    {
+        Vector3 dashDirection = rb.linearVelocity.normalized;
+        rb.AddForce(dashDirection * dashForce, ForceMode.Impulse);
+        canDash = false;
+        Invoke(nameof(ResetDash), dashCooldown);
+    }
+
+    private void ResetDash()
+    {
+        canDash = true;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
